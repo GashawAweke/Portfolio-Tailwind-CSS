3705f6fa6a82edb555df2dbe8b13327831732d15
GashawAweke <gashaye12aweke@gmail.com>
2025-03-09 17:58:35 +0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
+import About from './components/About';
+import Projects from './components/Projects';
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <NavBar />
       <Hero />
       <Skills />
+      <About />
+      <Projects />
     </>
   );
 };
